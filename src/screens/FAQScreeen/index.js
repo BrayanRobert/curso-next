b40c46f2e52fc1edc0cf2a339c4d843dbafe6f96
Antonio Oliveira <antoniooliveira@segmedic.com.br>
2022-10-05 08:11:27 -0300

    personalizando o FAQ

--- src/screens/FAQScreeen/index.js
+++ src/screens/FAQScreeen/index.js
@@ -4,7 +4,7 @@ import Link from '../../components/Link';
 import { theme } from '../../theme/theme';
 import { Box, Text } from '../../theme/components';
 
-export default function FAQScreen({ faq }) {
+export default function FAQScreen() {
   return (
     <Box
       styleSheet={{
@@ -47,7 +47,7 @@ export default function FAQScreen({ faq }) {
                 color:theme.colors.neutral[900],
               }}
             >
-              FAQ: Perguntas Frequentes
+            História do Batman
             </Text>
             <Text
               as="p"
@@ -57,9 +57,9 @@ export default function FAQScreen({ faq }) {
                 color: theme.colors.neutral[500],
               }}
             >
-              Não consegue encontrar a resposta que procura? entre em contato com nosso{' '}
+              Desenvolvido por Brayan Robert:{' '}
               <Link
-                href="mailto:contato@alura.com.br"
+                href="https://www.linkedin.com/in/brayan-robert-5b4b69203/"
                 styleSheet={{
                   color: theme.colors.primary[400],
                   hover: {
@@ -67,7 +67,7 @@ export default function FAQScreen({ faq }) {
                   },
                 }}
               >
-                time de suporte ao consumidor
+                clique aqui para acessar o linkedIn
               </Link>
             </Text>
 
@@ -103,55 +103,11 @@ export default function FAQScreen({ faq }) {
                 lg: "span 2 / span 2;",
               }
             }}
-          >
-            {faq.length === 0 && (
-              <Box
-                styleSheet={{
-                  display: 'flex',
-                  flexDirection: 'column',
-                  justifyContent: 'center',
-                  borderTop: {
-                    xs: `${theme.space.xpx} solid ${theme.colors.neutral["200"]}`,
-                    sm: 'none',
-                  },
-                  paddingTop: {
-                    xs: theme.space.x6,
-                    sm: 0,
-                  },
-                  borderLeft: {
-                    sm: `${theme.space.xpx} solid ${theme.colors.neutral["200"]}`,
-                  },
-                  paddingLeft: {
-                    sm: theme.space.x6,
-                  },
-                  textAlign: "center",
-                  minHeight: theme.space['x1/1']
-                }}
-              >
-                <Text
-                  as="h1"
-                  styleSheet={{
-                    textVariant: theme.typography.variants.heading1,
-                  }}
-                >
-                  Nada por aqui
-                </Text>
-                <Text
-                  as="p"
-                  styleSheet={{
-                    marginTop: theme.space.x1,
-                    textVariant: theme.typography.variants.body1,
-                    color: theme.colors.neutral[500],
-                  }}
-                >
-                  Talvez ainda não existam dúvidas frequentes.
-                </Text>
-              </Box>
-            )}
+          >          
             <Box as="dl">
-              {faq.map((faq) => (
+              {
                 <Box
-                  key={faq.question}
+                  
                   styleSheet={{
                     marginBottom: theme.space.x12,
                   }}
@@ -163,7 +119,7 @@ export default function FAQScreen({ faq }) {
                       color: theme.colors.neutral[900],
                     }}
                   >
-                    {faq.question}
+                    Quem é Batman?
                   </Text>
                   <Text
                     as="dd"
@@ -173,10 +129,52 @@ export default function FAQScreen({ faq }) {
                       color: theme.colors.neutral[500],
                     }}
                   >
-                    {faq.answer}
+                    Bruce Wayne é um bilionário americano, magnata de negócios, filantropo e dono da corporação Wayne Enterprises. Sua história teve início após testemunhar o assassinato de seus pais ainda quando criança, o que o fez jurar vingança contra os criminosos e treinar todos os dias sozinho, além de criar um personagem baseado em um morcego para combater o crime. Foi aí que surgiu Batman, o super-herói da cidade de Gotham.
                   </Text>
+                  <Text
+                    as="dt"
+                    styleSheet={{
+                      textVariant: theme.typography.variants.heading4,
+                      color: theme.colors.neutral[900],
+                    }}
+                  >
+                    Como ele combate o crime?
+                  </Text>
+                  <Text
+                    as="dd"
+                    styleSheet={{
+                      marginTop: theme.space.x2,
+                      textVariant: theme.typography.variants.body1,
+                      color: theme.colors.neutral[500],
+                    }}
+                  >
+                    Apesar de não ter superpoderes, Batman faz uso de seu intelecto, conhecimento em artes marciais, destreza física e habilidades de detetive. Mesmo tendo uma grande variedade de vilões como inimigos, incluindo o grande destaque, Coringa, Batman é ajudado por vários personagens incluindo o mordomo Alfred Pennyworth, o comissário de polícia Jim Gordon e outros aliados como Robin.
+                  </Text>
+                  <Text
+                    as="dt"
+                    styleSheet={{
+                      textVariant: theme.typography.variants.heading4,
+                      color: theme.colors.neutral[900],
+                    }}
+                  >
+                    Como Batman se mostra na sociedade?
+                  </Text>
+                  <Text
+                    as="dd"
+                    styleSheet={{
+                      marginTop: theme.space.x2,
+                      textVariant: theme.typography.variants.body1,
+                      color: theme.colors.neutral[500],
+                    }}
+                  >
+                    Bruce Wayne mostra-se à sociedade como um playboy irresponsável e superficial rico, porém ele também é conhecido por contribuir para a caridade através da Fundação Wayne para ajudar vítimas de crimes e impedir a entrada no crime de jovens. Essa personalidade foi criada para que as pessoas não desconfiassem de seu alter ego.
+
+                    Enquanto Bruce Wayne é despreocupado e irresponsável, Batman é frio, determinado e implacável. Além do uniforme e da máscara, quando Batman, Wayne altera sua voz para ficar cada vez mais irreconhecível.
+                  </Text>
+                  
                 </Box>
-              ))}
+                
+              }
             </Box>
           </Box>
         </Box>
