dea9637a6b08d4472e83e463cb09c60eaf5663da
Antonio Oliveira <antoniooliveira@segmedic.com.br>
2022-10-05 07:17:00 -0300

    adicionando página do faq

--- pages/faq.js
+++ pages/faq.js
@@ -1,6 +1,7 @@
-import Head from 'next/head'
 import { useEffect, useState } from 'react'
-import Link from '../src/components/Link'
+import FAQScreen from '../src/screens/FAQScreeen';
+
+export default FAQScreen;
 
 // getSideProps() Roda a cada acesso na página
 
@@ -18,29 +19,4 @@ export async function getStaticProps() {
             faq
         },
     }
-}
-
-export default function FAQPage({faq}) {
-
-    return (
-        <div>
-            <Head>
-                <title>Batman História</title>
-            </Head>
-            <h1>Alura Cases - Página de Perguntas FAQ</h1>
-            <Link href="/">
-                Ir para a home
-            </Link>
-            <ul>
-                {faq.map(({answer, question}) => (
-                    <li key={question}>
-                        <article>
-                            <h2>{question}</h2>
-                            <p>{answer}</p>
-                        </article>
-                    </li>
-                ))}
-            </ul>
-        </div>
-    )
 }
